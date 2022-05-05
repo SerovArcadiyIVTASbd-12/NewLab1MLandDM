aef6d8a9f093ad151318e045e49e1459ee7588b5
SerovArcadiyIVTASbd-12 <98806648+SerovArcadiyIVTASbd-12@users.noreply.github.com>
2022-05-05 23:16:54 +0300

    NewLab1MLandDM
    NewLab1MLandDM

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторные работы по МЛиДм</title>
+</head>
+<body>
+<a href="indexLab1.php">Первая лабораторная работа</a>
+
+
+
+</body>
+</html>
